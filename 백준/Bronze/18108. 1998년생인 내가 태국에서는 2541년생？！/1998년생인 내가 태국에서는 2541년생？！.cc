65381ab53c711b2885b901c4137a6ec2af7963a8
LYJ <yjsh221@gmail.com>
2024-09-12 02:14:14 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int bulYear = 0;
+
+	cin >> bulYear;
+
+	if (bulYear >= 1000 && bulYear <= 3000)
+	{
+		cout << bulYear - 543 << endl;
+	}
+}
